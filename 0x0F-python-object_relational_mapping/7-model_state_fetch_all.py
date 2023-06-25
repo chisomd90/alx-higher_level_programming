40c54833abbefb4698e8a641829d0cdbc07eebbc
chisomd90 <chisomd90@gmail.com>
2023-06-25 10:51:48 -0700

    ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Script that lists all objects from a database
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for states_id, states_name in session.query(State.id,
-                                                State.name).order_by(State.id):
-        print("{}: {}".format(states_id, states_name))
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
