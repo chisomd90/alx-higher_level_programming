8d9f61d853c00a0f08ea98e1e701579fbf7475fd
STONE <116025265+chisomd90@users.noreply.github.com>
2023-11-02 14:31:34 +0100

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/3-methods.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
